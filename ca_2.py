d55a1a50759e844aee8230b5eb76abe062cbcdd5
HARRISON\harrison <harrison.croaker@hotmail.com>
2018-05-10 17:27:15 +1000

    initial commit

---
+++ ca_2.py
@@ -0,0 +1,20 @@
+def evaluate(expression):
+    print("Evaluating expression")
+    # Do all the division first
+    # Do multiplication
+    # Do
+# Arithmetic Operators
+# * Multiply
+# / Integer divide
+# + Add
+# - Subtract
+
+# Binary Operators
+# & Bitwise AND
+# | Bitwise OR
+# ^ Bitwise XOR
+expression = "5/5/5"
+print(eval(expression))
+print(list(expression))
+answer = evaluate(expression)
+print(3+4/5*10)
