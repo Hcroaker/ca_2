96ebb900c5d6dfd6774e96fdf39044dbbda98c65
HARRISON\harrison <harrison.croaker@hotmail.com>
2018-05-12 13:55:57 +1000

    ev working

--- ca_2.py
+++ ca_2.py
@@ -1,20 +1,45 @@
-def evaluate(expression):
-    print("Evaluating expression")
-    # Do all the division first
-    # Do multiplication
-    # Do
-# Arithmetic Operators
-# * Multiply
-# / Integer divide
-# + Add
-# - Subtract
+"""
+Griffith Universiy Harrison Croaker 2018 (C)
 
-# Binary Operators
-# & Bitwise AND
-# | Bitwise OR
-# ^ Bitwise XOR
-expression = "5/5/5"
+"""
+
+operations = {
+    "+": lambda x, y: x + y,
+    "-": lambda x, y: x - y,
+    "/": lambda x, y: x // y,
+    "*": lambda x, y: x * y,
+    "%": lambda x, y: int(x % y),
+    "&": lambda x, y: int(x & y),
+    "|": lambda x, y: int(x | y),
+    "^": lambda x, y: int(x ^ y)
+}
+
+def doCalculations(expression, operator):
+    found = True
+    calculations = []
+    while found:
+        found = False
+        for i in range(len(expression)):
+            if expression[i]==operator:
+                found = True;
+                print(expression)
+                answer = operations[operator](int(expression[i-1]), int(expression[i+1]))
+                expression = expression[:i-1] + [str(answer)] + expression[i+2:len(expression)]
+                calculations.append(expression)
+                break;
+    return calculations
+
+def evaluate(expression):
+    expression = expression.split(" ")
+    print(expression)
+    operations = ["/", "%", "*", "+", "-", "&", "^", "|"]
+    for operator in operations:
+        res = doCalculations(expression, operator)
+        if res:
+            expression = res[-1]
+    return expression[0]
+
+expression = "28 * 9 + 5"
 print(eval(expression))
-print(list(expression))
 answer = evaluate(expression)
-print(3+4/5*10)
+print(answer)
